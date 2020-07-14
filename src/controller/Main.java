4cbcf084f4c7171ffbfbbc6c5f3315e636789691
saianuragt <saianuragt@gmail.com>
2020-07-14 10:38:59 +0530

    FSD0620B105

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,9 +5,16 @@ import java.sql.Connection;
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		
 	// Fill your code
+		Connection con = null;
+		con = ConnectionManager.getConnection();
+		
+		if(con != null)
+			System.out.println("Connection Established");
+		else
+			System.out.println("Connection not established");
 		
 	}
 }
